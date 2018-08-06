0fd0338a86fa749bdba4e64365045b885ef6b8ab
bruce <SuperBruceWon@163.com>
2018-08-06 10:51:59 +0800

    girl first Commit

---
+++ girl/src/main/java/com/example/girl/HiController.java
@@ -0,0 +1,28 @@
+package com.example.girl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+    @Autowired
+    private  GirlProperties girlProperties;
+
+
+
+
+//    @Value("${cupSize}")
+//    private String cupSize;
+//
+//    @Value("${age}")
+//    private Integer age;
+//
+//    @Value("${content}")
+//    private String content;
+
+    @GetMapping("/hello")
+    public String say(){
+        return girlProperties.getCupSize();
+    }
+}
